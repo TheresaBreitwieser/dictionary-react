db1da2670125a7491dd2812256b3ea97c0739920
TheresaBreitwieser <theresabreitwieser@gmail.com>
2021-10-19 07:24:57 +0200

    Built Search Engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello test
-      </header>
+      <div className="container">
+        <header className="App-header">
+          Dictionary
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Theresa Breitwieser, open-sourced on <a href="https://github.com/TheresaBreitwieser/dictionary-react" target="_blank" rel="noreferrer"> Github</a></footer>
+      </div>
     </div>
   );
 }
