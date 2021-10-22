ca578fb6ec04774835ed48301f345bcae8379364
TheresaBreitwieser <theresabreitwieser@gmail.com>
2021-10-22 08:25:57 +0200

    Display Results from Search Engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
-    const [keyword, setKeyword] = useState("");
+    let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function search(event) {
@@ -23,6 +25,7 @@ export default function Dictionary() {
             <form onSubmit={search}>
                 <input type="text" placeholder="Enter word here..." onChange={handleKeywordChange}/>
             </form>
+            <Results results={results}/>
         </div>
     );
 }
